907d90c6d8ef619232f4b153e3450b7c0defa5dd
Simon Gladman <sgladman@gmail.com>
2016-03-23 13:16:55 +0000

    Added alternative implementations using CIConvolution3X3 and hand-coded convolution to calculate Laplacian

--- CoreImageReactionDiffusion/ViewController.swift
+++ CoreImageReactionDiffusion/ViewController.swift
@@ -11,42 +11,17 @@ import GLKit
 
 class ViewController: UIViewController {
 
+    let grayScottFilter = GrayScottFilter()
+    
     let rect640x640 = CGRect(x: 0, y: 0, width: 640, height: 640)
     
     let accumulator = CIImageAccumulator(extent: CGRect(x: 0, y: 0, width: 640, height: 640), format: kCIFormatARGB8)
-    let grayScottFilter = GrayScottFilter()
     
     let edgesFilter = CIFilter(name: "CIEdges",
         withInputParameters: [kCIInputIntensityKey: 50])!
     
-    lazy var imageView: GLKView =
-    {
-        [unowned self] in
-        
-        let imageView = GLKView()
-        
-        imageView.layer.borderColor = UIColor.grayColor().CGColor
-        imageView.layer.borderWidth = 1
-        imageView.layer.shadowOffset = CGSize(width: 0, height: 0)
-        imageView.layer.shadowOpacity = 0.75
-        imageView.layer.shadowRadius = 5
-    
-        imageView.context = self.eaglContext
-        imageView.delegate = self
-        
-        return imageView
-    }()
-    
-    let eaglContext = EAGLContext(API: .OpenGLES2)
-    
-    lazy var ciContext: CIContext =
-    {
-        [unowned self] in
-        
-        return CIContext(EAGLContext: self.eaglContext,
-            options: [kCIContextWorkingColorSpace: NSNull()])
-    }()
-    
+    let imageView = MetalImageView()
+
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -55,7 +30,7 @@ class ViewController: UIViewController {
         
         setInitialImage()
         
-        let displayLink = CADisplayLink(target: self, selector: Selector("step"))
+        let displayLink = CADisplayLink(target: self, selector: #selector(ViewController.step))
         displayLink.addToRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)
     }
 
@@ -77,39 +52,151 @@ class ViewController: UIViewController {
     
     func step()
     {
-        imageView.setNeedsDisplay()
+        for _ in 0 ..< 5
+        {
+            grayScottFilter.setValue(accumulator.image(),
+                forKey: kCIInputImageKey)
+            accumulator.setImage(grayScottFilter.outputImage)
+        }
+        
+        edgesFilter.setValue(accumulator.image(),
+                             forKey: kCIInputImageKey)
+        
+        imageView.image = edgesFilter.outputImage!
     }
     
     override func viewDidLayoutSubviews()
     {
-        imageView.frame = CGRect(origin: CGPoint(x: view.frame.width / 2 - rect640x640.width / 2, y: view.frame.height / 2 - rect640x640.height / 2),
-            size: CGSize(width: rect640x640.width, height: rect640x640.height))
+        imageView.frame = CGRect(
+            origin: CGPoint(
+                x: view.frame.width / 2 - rect640x640.width / 2,
+                y: view.frame.height / 2 - rect640x640.height / 2),
+            size: CGSize(
+                width: rect640x640.width,
+                height: rect640x640.height))
     }
 
 }
 
-// MARK: GLKViewDelegate extension
 
-extension ViewController: GLKViewDelegate
+// MARK: GrayScottFilter_v2 - Uses Core Image convolution filter with Laplacian kernel
+class GrayScottFilter_v2: CIFilter
 {
-func glkView(view: GLKView, drawInRect rect: CGRect)
-{
-    for _ in 0 ..< 5
-    {
-        grayScottFilter.setValue(accumulator.image(),
-            forKey: kCIInputImageKey)
-        accumulator.setImage(grayScottFilter.outputImage)
-    }
+    var inputImage : CIImage?
+ 
+    var D_a: CGFloat = 0.189
+    var D_b: CGFloat = 0.080
+    var k: CGFloat = 0.062
+    var f: CGFloat = 0.0425
     
-    edgesFilter.setValue(accumulator.image(),
-        forKey: kCIInputImageKey)
+    let grayScottKernel = CIColorKernel(string:
+        "kernel vec4 coreImageKernel(__sample pixel, __sample laplacian, float D_a, float D_b, float k, float f) " +
+        "{" +
+        "float reactionRate = pixel.x * pixel.z * pixel.z;" +
+  
+        "float u = pixel.x + (D_a * laplacian.x) - reactionRate + f * (1.0 - pixel.x);" +
+        "float v = pixel.z + (D_b * laplacian.z) + reactionRate - (f + k) * pixel.z;" +
+        
+        "return vec4(u, u, v, 1.0);" +
+        "}"
+    )
 
-    ciContext.drawImage(edgesFilter.outputImage!,
-        inRect: CGRect(x: 0, y: 0,
-            width: imageView.drawableWidth,
-            height: imageView.drawableHeight),
-        fromRect: rect640x640)
+    let laplacianFilter: CIFilter =
+    {
+        let laplacianWeights = CIVector(values: [
+            CGFloat(0.0), CGFloat(1.0), CGFloat(0.0),
+            CGFloat(1.0), CGFloat(-4.0), CGFloat(1.0),
+            CGFloat(0.0), CGFloat(1.0), CGFloat(0.0) ], count: 9)
+        
+        return CIFilter(name: "CIConvolution3X3",
+            withInputParameters: [
+                kCIInputWeightsKey: laplacianWeights])!
+    }()
+    
+    override var outputImage : CIImage!
+    {
+        if let inputImage = inputImage,
+            grayScottKernel = grayScottKernel
+        {
+            laplacianFilter.setValue(inputImage,
+                forKey: kCIInputImageKey)
+  
+            let arguments = [inputImage,
+                laplacianFilter.outputImage!,
+                D_a, D_b, k, f]
+            let extent = inputImage.extent
+        
+            return grayScottKernel.applyWithExtent(extent,
+                arguments: arguments)
+        }
+        return nil
+    }
 }
+
+// MARK: GrayScottFilter_v3 - Hand coded convolution filter
+class GrayScottFilter_v3: CIFilter
+{
+    var inputImage : CIImage?
+    
+    var D_a: CGFloat = 0.189
+    var D_b: CGFloat = 0.080
+    var k: CGFloat = 0.062
+    var f: CGFloat = 0.0425
+    
+    let grayScottKernel = CIColorKernel(string:
+        "kernel vec4 coreImageKernel(__sample pixel, __sample laplacian, float D_a, float D_b, float k, float f) " +
+        "{" +
+        "float reactionRate = pixel.x * pixel.z * pixel.z;" +
+        
+        "float u = pixel.x + (D_a * laplacian.x) - reactionRate + f * (1.0 - pixel.x);" +
+        "float v = pixel.z + (D_b * laplacian.z) + reactionRate - (f + k) * pixel.z;" +
+        
+        "return vec4(u, u, v, 1.0);" +
+        "}"
+    )
+    
+    let laplacianKernel = CIKernel(string:
+        "kernel vec4 laplacianConvolution(sampler image) " +
+        "{" +
+        "vec2 d = destCoord();" +
+        
+        "vec2 northSample = sample(image, samplerTransform(image, d + vec2(0.0,-1.0))).rb;" +
+        "vec2 southSample = sample(image, samplerTransform(image, d + vec2(0.0,1.0))).rb;" +
+        "vec2 eastSample = sample(image, samplerTransform(image, d + vec2(1.0,0.0))).rb;" +
+        "vec2 westSample = sample(image, samplerTransform(image, d + vec2(-1.0,0.0))).rb;" +
+        
+        "vec2 thisSample = sample(image, samplerCoord(image)).xz;" +
+        
+        "vec2 laplacian = (northSample + southSample + eastSample + westSample) - (4.0 * thisSample);" +
+            
+        "return vec4(laplacian.x, laplacian.x, laplacian.y, 1.0);" +
+        "}"
+    )
+
+    override var outputImage : CIImage!
+    {
+        if let inputImage = inputImage,
+            laplacianKernel = laplacianKernel,
+            grayScottKernel = grayScottKernel
+        {
+            let laplacian = laplacianKernel.applyWithExtent(inputImage.extent,
+                roiCallback:
+                {
+                    (index, rect) in
+                    return rect
+                },
+                arguments: [inputImage])
+            
+            let arguments = [inputImage,
+                laplacian!,
+                D_a, D_b, k, f]
+            let extent = inputImage.extent
+        
+            return grayScottKernel.applyWithExtent(extent,
+                arguments: arguments)
+        }
+        return nil
+    }
 }
 
 // MARK: GrayScottFilter
@@ -148,22 +235,110 @@ class GrayScottFilter: CIFilter
         "}"
     )
     
-override var outputImage : CIImage!
-{
-    if let inputImage = inputImage,
-        grayScottKernel = grayScottKernel
+    override var outputImage : CIImage!
     {
-        let arguments = [inputImage, D_a, D_b, k, f]
-        let extent = inputImage.extent
-    
-        return grayScottKernel.applyWithExtent(extent,
-            roiCallback:
-            {
-                (index, rect) in
-                return rect
-            },
-            arguments: arguments)
+        if let inputImage = inputImage,
+            grayScottKernel = grayScottKernel
+        {
+            let arguments = [inputImage, D_a, D_b, k, f]
+            let extent = inputImage.extent
+        
+            return grayScottKernel.applyWithExtent(extent,
+                roiCallback:
+                {
+                    (index, rect) in
+                    return rect
+                },
+                arguments: arguments)
+        }
+        return nil
     }
-    return nil
 }
+
+// MARK Metal Image View
+
+import MetalKit
+
+/// `MetalImageView` extends an `MTKView` and exposes an `image` property of type `CIImage` to
+/// simplify Metal based rendering of Core Image filters.
+
+class MetalImageView: MTKView
+{
+    let colorSpace = CGColorSpaceCreateDeviceRGB()!
+    
+    lazy var commandQueue: MTLCommandQueue =
+    {
+        [unowned self] in
+        
+        return self.device!.newCommandQueue()
+    }()
+    
+    lazy var ciContext: CIContext =
+    {
+        [unowned self] in
+        
+        return CIContext(MTLDevice: self.device!)
+    }()
+
+    override init(frame frameRect: CGRect, device: MTLDevice?)
+    {
+        super.init(frame: frameRect,
+                   device: device ?? MTLCreateSystemDefaultDevice())
+        
+        if super.device == nil
+        {
+            fatalError("Device doesn't support Metal")
+        }
+        
+        framebufferOnly = false
+    }
+    
+    required init(coder: NSCoder)
+    {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    /// The image to display
+    var image: CIImage?
+        {
+        didSet
+        {
+            renderImage()
+        }
+    }
+    
+    func renderImage()
+    {
+        guard let
+            image = image,
+            targetTexture = currentDrawable?.texture else
+        {
+            return
+        }
+        
+        let commandBuffer = commandQueue.commandBuffer()
+        
+        let bounds = CGRect(origin: CGPointZero, size: drawableSize)
+        
+        let originX = image.extent.origin.x
+        let originY = image.extent.origin.y
+        
+        let scaleX = drawableSize.width / image.extent.width
+        let scaleY = drawableSize.height / image.extent.height
+        let scale = min(scaleX, scaleY)
+        
+        let scaledImage = image
+            .imageByApplyingTransform(CGAffineTransformMakeTranslation(-originX, -originY))
+            .imageByApplyingTransform(CGAffineTransformMakeScale(scale, scale))
+        
+        ciContext.render(scaledImage,
+                         toMTLTexture: targetTexture,
+                         commandBuffer: commandBuffer,
+                         bounds: bounds,
+                         colorSpace: colorSpace)
+        
+        commandBuffer.presentDrawable(currentDrawable!)
+        
+        commandBuffer.commit()
+    }
 }
